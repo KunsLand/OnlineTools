151e0dcb843097904b33022835bb132658f2bb34
likun <kunsland1990@gmail.com>
2015-07-08 00:03:09 +0800

    try web worker

---
+++ worker.js
@@ -0,0 +1,14 @@
+importScripts('core-min.js');
+importScripts('lib-typedarrays-min.js');
+importScripts('md5-min.js');
+importScripts('sha1-min.js');
+importScripts('sha224.js');
+importScripts('sha256-min.js');
+importScripts('sha384.js');
+importScripts('sha512.js');
+importScripts('sha3.js');
+
+
+self.addEventListener('message', function(evt){
+	self.postMessage(evt.data);
+}, false);
