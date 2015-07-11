d57f4045e4ffc0a671f1063692d4cf3988ad58fc
likun <kunsland1990@gmail.com>
2015-07-11 17:09:08 +0800

    split workers into different threads

--- sha2-512-worker.js
+++ sha2-512-worker.js
@@ -5,13 +5,33 @@ importScripts('sha512.js');
 self.algo = CryptoJS.algo.SHA512.create();
 
 self.addEventListener('message', function(evt){
-	var wordArr = CryptoJS.lib.WordArray.create(evt.data.blob);
-	self.algo.update(wordArr);
-	out = {'chunk':evt.data.chunk, 'chunks':evt.data.chunks};
-	if(evt.data.chunk==evt.data.chunks){
-		out.result = self.algo.finalize().toString();
-	}
-	evt = null;
-	wordArr = null;
-	self.postMessage(out);
+	var file, fileReader, start, end, chunk, chunks, chunkSize, currentChunk;
+    handle_load_blob=function(e){
+		var wordArr = CryptoJS.lib.WordArray.create(e.target.result);
+		self.algo.update(wordArr);
+		out = {'chunk':++currentChunk, 'chunks':chunks};
+		if(currentChunk==chunks){
+			out.result = self.algo.finalize().toString();
+		}
+		self.postMessage(out);
+
+		if(end!==file.size){
+			fileReader = new FileReader();
+			fileReader.onload = handle_load_blob;
+			start = currentChunk * chunkSize;
+			end = ((start + chunkSize) >= file.size) ? file.size : start + chunkSize;
+			fileReader.readAsArrayBuffer(file.slice(start, end));
+		}
+    }
+    file = evt.data;
+    currentChunk = 0;
+    chunkSize = 2097152; // read in chunks of 2MB
+    chunks = Math.ceil(file.size / chunkSize);
+
+	start = currentChunk * chunkSize;
+	end = ((start + chunkSize) >= file.size) ? file.size : start + chunkSize;
+
+	fileReader = new FileReader();
+	fileReader.onload = handle_load_blob;
+	fileReader.readAsArrayBuffer(file.slice(start, end));
 }, false);
