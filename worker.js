eb5f9e47b2498ab9ce4c854562e106df5e621173
likun <kunsland1990@gmail.com>
2015-07-11 13:23:20 +0800

    support hash algorithm with web worker

--- worker.js
+++ worker.js
@@ -8,6 +8,12 @@ importScripts('sha384.js');
 importScripts('sha512.js');
 importScripts('sha3.js');
 
+self.md5 = CryptoJS.algo.MD5.create();
+
 self.addEventListener('message', function(evt){
+	self.md5.update(CryptoJS.lib.WordArray.create(evt.data.blob));
+	if(evt.data.chunk==evt.data.chunks){
+		evt.data.result = self.md5.finalize();
+	}
 	self.postMessage(evt.data);
 }, false);
