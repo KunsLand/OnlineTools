cf4989bfce7454531eac99bce88d7bf26c2ea1cd
likun <kunsland1990@gmail.com>
2015-07-12 13:37:43 +0800

    fix oom bugs

--- worker.js
+++ worker.js
@@ -1,9 +1,9 @@
 importScripts('core-min.js');
 importScripts('lib-typedarrays-min.js');
-importScripts('md5-min.js');
-importScripts('sha1-min.js');
+importScripts('md5.js');
+importScripts('sha1.js');
 importScripts('sha224.js');
-importScripts('sha256-min.js');
+importScripts('sha256.js');
 importScripts('sha384.js');
 importScripts('sha512.js');
 importScripts('sha3.js');
